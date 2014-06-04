8c9e4fadd4d6862f523025d398569a90699ec49b
Smooth P <pdo400@yahoo.com>
2014-06-04 10:20:56 -0700

    Moved known extractions to ScraperSpec.  Instantiate RuleManager with parser pool.

--- src/main/scala/patrickw/RuleManager.scala
+++ src/main/scala/patrickw/RuleManager.scala
@@ -20,20 +20,8 @@ object RuleManager extends scalaz.Options {
   final val SnapshotPeriod: Long = 10 * (1000 * 60)
   final val SnapshotStaleTime: Long = 24 * (1000 * 60 * 60)
 
-  val known: KnownExtractions = Map(
-      (new URL("https://twitter.com/camplejohn"), Map(Name -> "Doug Camplejohn")),
-      (new URL("https://twitter.com/pdo400"), Map(Name -> "Patrick Whitesell")),
-      (new URL("https://twitter.com/runarorama"), Map(Name -> "Rúnar Óli")),
-      (new URL("https://twitter.com/BillyBobThorntn"), Map(Name -> "Billy Bob Thornton")),
-      (new URL("https://twitter.com/ladygaga"), Map(Name -> "Lady Gaga")),
-      (new URL("https://twitter.com/SFGiants"), Map(Name -> "San Francisco Giants")),
-      (new URL("https://twitter.com/49ers"), Map(Name -> "San Francisco 49ers")),
-      (new URL("https://twitter.com/WSJ"), Map(Name -> "Wall Street Journal")),
-      (new URL("https://twitter.com/Fliptop"), Map(Name -> "Fliptop")),
-      (new URL("https://twitter.com/scala_lang"), Map(Name -> "Scala"))
-    )
-
-  def apply() = new RuleManager(known, Parser.TagSoup)
+  def apply(knownExtractions: KnownExtractions, parserPool: Parser.Pool) =
+    new RuleManager(knownExtractions, parserPool)
 }
 
 class RuleManager(knownExtractions: KnownExtractions, parserPool: Parser.Pool) {
--- src/test/scala/patrickw/ScraperSpec.scala
+++ src/test/scala/patrickw/ScraperSpec.scala
@@ -28,11 +28,24 @@ object ScraperSpec {
   val sampleExtraction: Extraction = Map(Name -> "Doug Camplejohn")
   val remoteUrl = new URL("https://twitter.com/camplejohn")
 
+  val knownExtractions: KnownExtractions = Map(
+    (new URL("https://twitter.com/camplejohn"), Map(Name -> "Doug Camplejohn")),
+    (new URL("https://twitter.com/pdo400"), Map(Name -> "Patrick Whitesell")),
+    (new URL("https://twitter.com/BillyBobThorntn"), Map(Name -> "Billy Bob Thornton")),
+    (new URL("https://twitter.com/ladygaga"), Map(Name -> "Lady Gaga")),
+    (new URL("https://twitter.com/SFGiants"), Map(Name -> "San Francisco Giants")),
+    (new URL("https://twitter.com/49ers"), Map(Name -> "San Francisco 49ers")),
+    (new URL("https://twitter.com/WSJ"), Map(Name -> "Wall Street Journal")),
+    (new URL("https://twitter.com/linuxfoundation"), Map(Name -> "The Linux Foundation")),
+    (new URL("https://twitter.com/Fliptop"), Map(Name -> "Fliptop")),
+    (new URL("https://twitter.com/scala_lang"), Map(Name -> "Scala"))
+  )
+
   val testExtractions: KnownExtractions = Map(
+    (new URL("https://twitter.com/runarorama"), Map(Name -> "Rúnar Óli")),
     (new URL("https://twitter.com/ubuntucloud"), Map(Name -> "Ubuntu Cloud")),
     (new URL("https://twitter.com/eucalyptus"), Map(Name -> "Eucalyptus")),
     (new URL("https://twitter.com/typesafe"), Map(Name -> "Typesafe")),
-    (new URL("https://twitter.com/linuxfoundation"), Map(Name -> "The Linux Foundation")),
     (new URL("https://twitter.com/googlemaps"), Map(Name -> "Google Maps")),
     (new URL("https://twitter.com/TEDTalks"), Map(Name -> "TED Talks")),
     (new URL("https://twitter.com/RedSox"), Map(Name -> "Boston Red Sox")),
@@ -113,7 +126,7 @@ class ScraperSpec(parserPool: Parser.Pool) extends FlatSpec with Matchers {
     }
 
     "RuleManager" should "correctly extract each remote test document" in {
-      val rm = RuleManager()
+      val rm = RuleManager(knownExtractions, parserPool)
 
       for ((url, ex) <- testExtractions)
         rm.extractAndEvolve(url).unweighted should be (ex)
